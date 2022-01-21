7814f0aa7bf2bbca1d4ed8ae64e43876013aedbd
Girish Malekar <gmalekar@hawk.iit.edu>
2022-01-21 10:10:15 -0600

    Create Data Driven Decision Making with advanced SQL queries.sql

---
+++ SQL For Business Analyst/Data-Driven Decision Making in SQL/Data Driven Decision Making with advanced SQL queries.sql
@@ -0,0 +1,8 @@
+--The concept of nested queries and correlated nested queries is introduced and the functions EXISTS and UNION are used to categorize customers, movies, actors, and more.
+
+/***
+Often rented movies
+Your manager wants you to make a list of movies excluding those which are hardly ever watched. 
+This list of movies will be used for advertising. List all movies with more than 5 views using a nested query which is a powerful tool to implement selection conditions.
+***/
+
