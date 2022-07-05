9184d838a67828becea4a08606543f8378342284
Girish Malekar <gmalekar@hawk.iit.edu>
2022-07-04 20:25:38 -0500

    Create Best Practices for Writing SQL.sql

---
+++ SQL For Business Analyst/Applying SQL to Real-World Problems/Best Practices for Writing SQL.sql
@@ -0,0 +1,5 @@
+/***
+Best Practices for Writing SQL
+How do you ensure that the SQL scripts you write will be easy to understand for anyone who needs to read them? 
+This chapter will cover approaches you can leverage to ensure that your code clearly conveys your intent, is readable by others and follows best practices.
+***/
